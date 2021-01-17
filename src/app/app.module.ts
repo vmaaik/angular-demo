1121bdd3942a1f8803f12ebacdb7602ec6ab95e8
michal gebarowski <vmaaik@gmail.com>
2021-01-17 14:00:21 +0000

    set up a course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,9 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from './success-alert/success-alert.component';
-import {WarningComponent} from './warning/warning-component';
-import {ServerComponent} from './server/server.component';
-import {ServersComponent} from './servers/servers.component';
-import { AssignmentSecondComponent } from './assignment-second/assignment-second.component';
-import { AssignementThirdComponent } from './assignement-third/assignement-third.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    WarningComponent,
-    ServerComponent,
-    ServersComponent,
-    AssignmentSecondComponent,
-    AssignementThirdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
