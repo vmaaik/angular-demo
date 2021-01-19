3c8e65acf4de11fb2f9ba0454ef11c20b2145404
michal gebarowski <vmaaik@gmail.com>
2021-01-19 12:52:22 +0000

    add app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+import {HeaderComponent} from './header/header.component';
+import {RecipeItemComponent} from './recipe_book/recipe_item/recipe-item.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipiesComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
