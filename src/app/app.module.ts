f9a2d109284cfe3506409429954782790e7b317f
michal gebarowski <vmaaik@gmail.com>
2021-01-19 14:20:57 +0000

    update components' position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+
 import {HeaderComponent} from './header/header.component';
-import {RecipeItemComponent} from './recipe_book/recipe_item/recipe-item.component';
+
 import { RecipiesComponent } from './recipies/recipies.component';
 import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {RecipeDetailComponent} from './recipies/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipies/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
